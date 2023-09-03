09ca83572c9072a13188c7b2bffb689509b8bf3e
Francis Ali <francisali692@gmail.com>
2023-09-03 13:53:25 +0100

    Changed mail_config.py

--- mail_config.py
+++ mail_config.py
@@ -1,8 +1,11 @@
+import os
+
+
 mail_config = {
 	'MAIL_SERVER': 'smtp.gmail.com',
 	'MAIL_PORT': 465,
 	'MAIL_USERNAME': 'dummy42113@gmail.com',
-	'MAIL_PASSWORD': 'dummylistic',
+	'MAIL_PASSWORD': os.environ.get('MAIL_PASSWORD'),
 	'MAIL_USE_TLS': False,
 	'MAIL_USE_SSL': True
 }
