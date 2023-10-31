68f5dea0c9fc6ed5ab3d284780254ed878733400
Francis Ali <francisali692@gmail.com>
2023-10-31 15:22:57 +0100

    minor changes

--- mail_config.py
+++ mail_config.py
@@ -2,8 +2,8 @@ import os
 
 
 mail_config = {
-	'MAIL_SERVER': 'smtp.gmail.com',
-	'MAIL_PORT': 465,
+	'MAIL_SERVER': 'smtp.mail.yahoo.com',
+	'MAIL_PORT': 587,
 	'MAIL_USERNAME': 'dummy42113@gmail.com',
 	'MAIL_PASSWORD': os.environ.get('MAIL_PASSWORD'),
 	'MAIL_USE_TLS': False,
