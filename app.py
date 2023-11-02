4187288a51a6b3605b246357c71ae7c9554d2f25
Francis Ali <francisali692@gmail.com>
2023-11-02 18:50:01 +0100

    changes made

--- app.py
+++ app.py
@@ -1,32 +1,51 @@
 import sys
 import os
+from smtplib import SMTP
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+os.environ['RECIPIENT_EMAIL'] = 'francisali692@gmail.com'
+os.environ['MAIL_PASSWORD'] = 'U5T3G9ZX6MLKfOdq'
 
 from flask import Flask, render_template, request
-from flask_mail import Mail, Message
 from mail_config import mail_config
 
 
 app = Flask(__name__)
-app.config.update(mail_config)
-
-mail = Mail(app)
-
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
+	sender_email = mail_config.get('MAIL_USERNAME')
+	recipient_email = os.getenv('RECIPIENT_EMAIL')
+	
 	if request.method == 'POST':
 		values = request.values
-		message = Message(
-			"E-mail from {values.get('name')}",
-			sender = mail_config.get('MAIL_USERNAME'),
-			recipients = [os.getenv("RECIPIENT_EMAIL")]
-		)
-		message.body = f'''
+		print(values.get('name'))
+		print(values.get('email'))
+		print(values.get('message'))
+		msg = MIMEMultipart()
+		msg['From'] = mail_config.get('MAIL_USERNAME')
+		msg['To'] = os.getenv('RECIPIENT_EMAIL')
+		msg['Subject'] = f"E-mail from {values.get('name')}"
+
+		body = f'''
 			{values.get('message')}
 
-			My email address: {values.get('email')}.
-
 			{values.get('name')}.
+
+			
+			Give a feedback to {values.get('name')} at {values.get('email')}.
 			'''
-		mail.send(message)
+		msg.attach(MIMEText(body, 'plain'))
+
+		# Start the SMTP session
+		server = SMTP(
+			mail_config.get('MAIL_SERVER'), 
+			mail_config.get('MAIL_PORT')
+		)
+		server.starttls()
+		server.login(sender_email, mail_config.get('MAIL_PASSWORD'))
+
+		server.sendmail(sender_email, recipient_email, msg.as_string())
+		server.quit()
 	return render_template('form.html')
